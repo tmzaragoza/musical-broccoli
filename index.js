48fb74e6eb1ac1c74aff6a62d5b1f5d5aa6eb3ac
TEONNA ZARAGOZA <TEONNA.ZARAGOZA@GMAIL.COM>
2018-10-03 20:43:04 -0700

    show prompt and confirm

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('hello bc preppers');
+var countTo = prompt( 'how high should we count even numbers?');
+
+var sure = confirm('are you sure you want to count to' + countTo);
+
+if(sure) {
+    for(var i = 0; i < countTo; i++) {
+        var remainder = i % 2
+        if(remainder === 0) {
+            console.log(i, 'is even');
+    }
+}
+}    
